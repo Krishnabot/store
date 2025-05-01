8446b983b7559aa2656d46ad7eca95379cf4451d
Krishna <libraryismyparadise@gmail.com>
2025-05-01 16:17:09 +0900

    Add asset manifest configuration for images, stylesheets, javascripts, and fonts

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,8 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+//= link_directory ../javascripts .js
+//= link_directory ../fonts .woff2
+//= link_directory ../fonts .woff
+//= link_directory ../fonts .ttf
+//= link_directory ../fonts .eot
+//= link_directory ../fonts .svg
